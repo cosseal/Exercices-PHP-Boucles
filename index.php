3f8f2d2570c5d9ecef99c297cfa2a65a84613ab5
sstienface <s.stien@fondationface.org>
2018-12-05 12:02:12 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sstienface
+ * Date: 04/12/2018
+ * Time: 11:25
+ */
+
+// Premiere ligne
+$i = 1;
+
+($i < 6)
+  echo $i;
+  $i++;
+
+//Deuxieme ligne
+echo"<br><br>";
+
+$i = 1;
+
+ {
+     echo $i;
+     $i++;
+ }  ($i < 6);
+
+echo"<br><br>";
+//Troisieme ligne
+ ($i = 0; $i < 10; ) {
+    echo $x;
+}
+
+
+echo"<br><br>";
+//Quatrieme ligne
+$colors = array("red", "green", "blue", "yellow");
+
+ ($colors  $x) {
+    echo $x;
+}
+
+
